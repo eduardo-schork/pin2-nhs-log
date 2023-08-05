e6870f9f95f6fc1fab1831f91850398817c61da5
Eduardo Schork <eduardo.schork1814@gmail.com>
2023-08-05 14:44:14 -0300

    chore: change front end project to vite standard setup

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@/Assets', replacement: '/src/Assets' },
-      { find: '@/Components', replacement: '/src/Components' },
-    ],
-  }
 })
